845f3a4ba648af7fa93348065a898f96657726ce
Ahmed Abdullahi <111780020+axmed-cs@users.noreply.github.com>
2024-07-16 16:33:50 +0000

    initial

--- welcome.c
+++ welcome.c
@@ -6,8 +6,13 @@ int main(int argc, char** argv) {
     printf("Try running with ./welcome your-name\n");
     return 1;
   }
+  
   char* name = argv[1];
   int length = strlen(name);
   printf("Hi %s, your name is %d characters long according to strlen.\n", name, length);
+
+  char firstLetter = name[0];
+  printf("The first character of your name is %c \n", firstLetter);
+
   return 0;
 }
